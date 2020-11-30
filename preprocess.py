a29f972b77d7a10400b538cd0ab5ee44181c43e6
Ridvan <ridvan.cetin@nordakademie.de>
2020-11-30 14:56:54 +0100

    entwicklung des preprocesses

--- preprocess.py
+++ preprocess.py
@@ -1,2 +1,15 @@
-def preprocess():
+import csv
+
+def main():
+	base_path = "/Sandboxes/spam-detection-WPK-2020"
 	data_file_path = "spam_task_train.csv"
+	data_file_delimiter = "\t"
+	csv_reader = csv.reader(open(data_file_path, encoding='utf-8'), delimiter=data_file_delimiter)
+	data_lines = [line for line in csv_reader]
+	
+	for line in data_lines:
+		data_text = line[0]
+		print(data_text)
+
+if __name__ == "__main__":
+	main()
